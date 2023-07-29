fafd361c82bfa18143b70b114143b935dae3ff25
Qaviashraf <qaviashraf100@gmail.com>
2023-07-30 00:02:05 +0500

    Done

--- api.js
+++ api.js
@@ -15,26 +15,56 @@ let students = [
 // GET all students
 app.get("/api/students", (req, res) => {
   // Write your code here
+  res.json(students);
 })
 
 // POST student
 app.post("/api/students", (req, res) => {
   // Write your code here
+  const { name } = req.body;
+  const id = students.length;
+  const newStudent = { id, name };
+  students.push(newStudent);
+  res.status(201).json(newStudent);
 })
 
 // PUT student
 app.put("/api/students/:id", (req, res) => {
   // Write your code here
+  const id = parseInt(req.params.id);
+  const { name } = req.body;
+
+  const studentToUpdate = students.find((student) => student.id === id);
+
+  if (!studentToUpdate) {
+    return res.status(404).json({ message: "Student not found" });
+  }
+  studentToUpdate.name = name;
+  res.json(studentToUpdate);
 })
 
 // DELETE student
 app.delete("/api/students/:id", (req, res) => {
   // Write your code here
+  const id = parseInt(req.params.id);
+
+  const studentIndex = students.findIndex((student) => student.id === id);
+
+  students.splice(studentIndex, 1);
+  res.json({ message: "Student deleted successfully." });
 })
 
 // GET one student
 app.get("/api/students/:id", (req, res) => {
   // Write your code here
+  const id = parseInt(req.params.id);
+  const student = students.find((student) => student.id === id);
+
+  if (!student) {
+    return res.status(404).json({ message: "Student not found" });
+  }
+
+  res.json(student);
 })
 
 // app.listen(3000, () => {
