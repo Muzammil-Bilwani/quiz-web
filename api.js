7118238c3682571a3f07ab5ab4a236018b08b08d
mohiburrehmanniazi <126156723+mohiburrehmanniazi@users.noreply.github.com>
2023-07-30 21:26:38 +0500

    Assignment 11

--- api.js
+++ api.js
@@ -14,31 +14,40 @@ let students = [
 
 // GET all students
 app.get("/api/students", (req, res) => {
-  // Write your code here
+  res.json(students)
 })
 
 // POST student
 app.post("/api/students", (req, res) => {
-  // Write your code here
+  const newStudent = {
+    id: 1,
+    name: "Mohib"
+  }
+  students.push(newStudent)
+  res.json(students)
 })
 
 // PUT student
 app.put("/api/students/:id", (req, res) => {
-  // Write your code here
+  const student = students.find(s => s.id === 1)
+  student.name = "mujeeb"
+  res.json(students)
 })
 
 // DELETE student
 app.delete("/api/students/:id", (req, res) => {
-  // Write your code here
+  students = students.find(s => s.id !== 1)
+  res.json(students)
 })
 
 // GET one student
 app.get("/api/students/:id", (req, res) => {
-  // Write your code here
+  const student = students.find(s => s.id === 0)
+  res.json(student)
 })
 
-// app.listen(3000, () => {
-//   console.log("Server listening on port 3000")
-// })
+app.listen(3000, () => {
+  console.log("Server listening on port 3000")
+})
 
 module.exports = app
