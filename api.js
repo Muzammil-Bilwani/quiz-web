80f366284ad60c1a2d66138c40f5260f650edf89
Hasnain911 <ratios.rate@gmail.com>
2023-08-01 17:45:46 +0500

    assignment done.
    All tests passed.

--- api.js
+++ api.js
@@ -1,44 +1,68 @@
-const express = require("express")
-const app = express()
+const express = require("express");
+const app = express();
 
 // Middleware to parse request body as JSON
-app.use(express.json())
+app.use(express.json());
 
 // Sample data - replace this with an actual database or data storage
 let students = [
-  {
-    id: 0,
-    name: "John Marry"
-  }
-]
+  { "id": 0, "name": "John Marry" },
+  { "id": 1, "name": "Marry" },
+];
 
 // GET all students
 app.get("/api/students", (req, res) => {
-  // Write your code here
-})
+  res.json(students);
+});
+
 
 // POST student
 app.post("/api/students", (req, res) => {
-  // Write your code here
-})
+  const { id, name } = req.body;
+  const newStudent = { id, name };
+  const existingStudent = students.find((student) => student.id === newStudent.id);
+
+  if (!existingStudent) {
+    return res.json({ message: "Student with the same id exists" });
+  } else {
+    students.push(newStudent);
+    return res.status(201).json(newStudent);
+  }
+});
+
 
 // PUT student
 app.put("/api/students/:id", (req, res) => {
-  // Write your code here
+  const id = req.params.id;
 })
 
 // DELETE student
 app.delete("/api/students/:id", (req, res) => {
-  // Write your code here
-})
+  const studentIdToBeExpelled = parseInt(req.params.id);
+  const expelledStudentIndex = students.findIndex((student) => student.id == studentIdToBeExpelled)
+ 
+students.splice(expelledStudentIndex, 1);
+res.json({ message: "Student deleted successfully." });
+
+});
+
+
 
 // GET one student
 app.get("/api/students/:id", (req, res) => {
-  // Write your code here
-})
+  const studentId = parseInt(req.params.id);
+  const singleStudent = students.find((student) => student.id == studentId);
 
-// app.listen(3000, () => {
-//   console.log("Server listening on port 3000")
-// })
+  if (!singleStudent) {
+    return res.status(404).json({ message: "Student not found" });
+  }
 
-module.exports = app
+  res.json(singleStudent);
+});
+
+
+app.listen(3000, () => {
+  console.log("Server listening on port 3000");
+});
+
+module.exports = app;
