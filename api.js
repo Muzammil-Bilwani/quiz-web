52295c955dfd4e4539a72046e206311583539079
AdilAhmedShekhani <126003246+AdilAhmedShekhani@users.noreply.github.com>
2023-08-01 17:25:08 +0500

    API Assignment

--- api.js
+++ api.js
@@ -14,22 +14,46 @@ let students = [
 
 // GET all students
 app.get("/api/students", (req, res) => {
-  // Write your code here
+   res.json(students);
 })
 
 // POST student
 app.post("/api/students", (req, res) => {
-  // Write your code here
+  const { name } = req.body;
+  const newStudentId = Math.max(...students.map(student => student.id)) + 1;
+  const newStudent = {
+    id: newStudentId,
+    name: name,
+  };
+  students.push(newStudent);
+  res.json(newStudent);
 })
 
 // PUT student
 app.put("/api/students/:id", (req, res) => {
-  // Write your code here
+  const studentId = parseInt(req.params.id, 10);
+  const { name } = req.body;
+  //find the students from array
+  const studentIndex = students.findIndex(student => student.id === studentId);
+  if (studentIndex === -1) {
+    //return error when students not found
+    return res.status(404).json({ message: 'Student not found' });
+  }
+  // Update Student Name
+  students[studentIndex].name = name;
+  //Return with updated student name
+  res.json(students[studentIndex]);
 })
 
 // DELETE student
 app.delete("/api/students/:id", (req, res) => {
-  // Write your code here
+  const studentId = parseInt(req.params.id, 10);
+  const studentIndex = students.findIndex(student => student.id === studentId);
+  if (studentIndex === -1) {
+    return res.status(404).json({ message: 'Student not found' });
+  }
+  students.splice(studentIndex, 1);
+  res.json({ message: 'Student deleted successfully' });
 })
 
 // GET one student
