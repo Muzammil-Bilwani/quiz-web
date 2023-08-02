84c699278cf00d58dc226491b27b8c17be02c08d
AdilAhmedShekhani <126003246+AdilAhmedShekhani@users.noreply.github.com>
2023-08-02 16:54:51 +0500

    API Test

--- api.js
+++ api.js
@@ -20,49 +20,58 @@ app.get("/api/students", (req, res) => {
 // POST student
 app.post("/api/students", (req, res) => {
   const { name } = req.body;
-  const newStudentId = Math.max(...students.map(student => student.id)) + 1;
-  const newStudent = {
-    id: newStudentId,
-    name: name,
-  };
+  if (!name || !name.trim()) {
+    return res.status(400).json({ message: "invalid input" });
+  }
+  const id = students.length;
+  const newStudent = { id, name };
   students.push(newStudent);
-  res.json(newStudent);
-})
+  return res.status(201).json(newStudent);
+});
 
 // PUT student
 app.put("/api/students/:id", (req, res) => {
-  const studentId = parseInt(req.params.id, 10);
+  const id = parseInt(req.params.id);
   const { name } = req.body;
-  //find the students from array
-  const studentIndex = students.findIndex(student => student.id === studentId);
-  if (studentIndex === -1) {
-    //return error when students not found
-    return res.status(404).json({ message: 'Student not found' });
+
+  const student = students.find((student) => student.id === id);
+
+  if (!student) {
+    return res.status(404).json({ message: "Student not found" });
   }
-  // Update Student Name
-  students[studentIndex].name = name;
-  //Return with updated student name
-  res.json(students[studentIndex]);
-})
+
+  student.name = name;
+  return res.json(student);
+});
 
 // DELETE student
 app.delete("/api/students/:id", (req, res) => {
-  const studentId = parseInt(req.params.id, 10);
-  const studentIndex = students.findIndex(student => student.id === studentId);
-  if (studentIndex === -1) {
-    return res.status(404).json({ message: 'Student not found' });
+  const id = parseInt(req.params.id);
+
+  const studentInd = students.findIndex((student) => student.id === id);
+
+  if (studentInd === -1) {
+    return res.status(404).json({ message: "Student not found" });
   }
-  students.splice(studentIndex, 1);
-  res.json({ message: 'Student deleted successfully' });
-})
+
+  students.splice(studentInd, 1);
+  return res.json({ message: "Student deleted successfully." });
+});
 
 // GET one student
 app.get("/api/students/:id", (req, res) => {
-  // Write your code here
-})
+  const id = parseInt(req.params.id);
+  const student = students.find((student) => student.id === id);
+
+  if (!student) {
+    return res.status(404).json({ message: "Student not found" });
+  }
+
+  return res.json(student);
+});
 
 // app.listen(3000, () => {
 //   console.log("Server listening on port 3000")
 // })
 
-module.exports = app
+module.exports = app;
